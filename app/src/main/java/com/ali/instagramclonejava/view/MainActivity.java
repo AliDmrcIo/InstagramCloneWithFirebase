15a1febf93a652b7cf8ded1977e63b860ceda705
AliDemirci <110434358+AliBaba61@users.noreply.github.com>
2023-06-03 21:44:15 +0300

    Update MainActivity.java

--- app/src/main/java/com/ali/instagramclonejava/view/MainActivity.java
+++ app/src/main/java/com/ali/instagramclonejava/view/MainActivity.java
@@ -18,7 +18,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private FirebaseAuth auth;//firebase Authentication sınıfını kullanılmak üzere hazırda beklettim
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +28,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
 
 
-        auth = FirebaseAuth.getInstance();//firebase'i initialize ettik. objeyi oluşturduk. Artık bunu kullanarak giriş çıkış işlemlerinin hepsini yapabilirim.
+        auth = FirebaseAuth.getInstance();
 
-        FirebaseUser user=auth.getCurrentUser();//daha önceden giriş yapmış kullanıcı varsa onu bana verir ama kesin değil . Nullable diyoruz buna, olabilir olmayadabilir o yüzden şimdi if açıp eğer giriş yapan varsa tekrar ona email şifre kısmını gösterme dicez
+        FirebaseUser user=auth.getCurrentUser();
 
-        if(user!=null){//eğer halihazırda uygulamada giriş yapmış kullanıcı varsa
+        if(user!=null){
             Intent intent=new Intent(MainActivity.this,FeedActivity.class);
             startActivity(intent);
             finish();
 
         }
-
-        //Normalde büyük bir firmada ekipler olur. Bu ekiplerde backendcisi olur, dizayncısı, database cisi vs olur. Firebase bize bunları tek kişiyle yapabilme olanağı sunuyor. Firebase aslında verileri internette tutup kullanıcının giriş bilgilerine girdikten sonra verilerine erişebileceği bir hizmet sunar. Firebase çok büyük bir backend servisidir gibi düşünebiliriz. İçerisinde veritabanı hizmeti, depolama, kullanıcı oluşturma, parolaları saklama, parolaları oluşturma, hesaba giriş yapma. Bunların hepsini bize sağlayan bir Google servisidir. İlk başta ücretsiz bir hizmettir ancak uygulama belli bir popülerliğe ulaştığı zaman kullandığın kadar öde mantığıyla çalışmaya başlıyor. Firebasein hizmetlerinde analizler gibi kısımlar da var, kullanıcılar hangi ülkelerden, cinsiyet dağılımları ne vs gibi. Firebase hiç kullanmasa bile bir firma, analizler için kullanabiliyormuş. Firebase; Android, Ios, web, unity, vb platformlarda çalışabiliyor
-        //https://console.firebase.google.com/project/instaclonejava-5df97/overview buraya gidip paket ismini yapıştırıp bize sunduğu dosyayı indirmemiz ve burada androidden çıkıp project'e  tıklayıp app içerisine o dosyayı atmamız gerekiyor.
-
 
     }
 
@@ -49,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    public void signInClicked(View view){//giriş yap
+    public void signInClicked(View view){
 
         String email=binding.emailText.getText().toString();
         String password=binding.passwordText.getText().toString();
@@ -61,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
 
-            auth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() //email ve parolayla giriş yap. Aşağıdaki auth.createUser.... kısmıyla aynı zaten listener falan olarakta. Bilgi almak için aşağı bak
+            auth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>()
             {
                 @Override
                 public void onSuccess(AuthResult authResult) {
@@ -81,32 +77,32 @@ public class MainActivity extends AppCompatActivity {
         }
 }
 
-    public void signUpClicked(View view){//kayıt ol
+    public void signUpClicked(View view){
 
         String email=binding.emailText.getText().toString();
         String password=binding.passwordText.getText().toString();
 
-        if((email.equals("") || password.equals(""))){//kullanıcı eğer email veya şifreden herhangi birini boş bıraktıysa ona toast mesajı gönderiyoruz.
+        if((email.equals("") || password.equals(""))){
             Toast.makeText(MainActivity.this, "Lutfen Email ve Sifreyi bos birakmayiniz!", Toast.LENGTH_LONG).show();
         }
 
-        else{//email ve şifre kısmını kullanıcının boş bırakmadığından emin olmak istiyoruz.
+        else{
 
             auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {//email ve şifre ile bir kullanıcı yarat dedik. İçerisinde de bizden bir email ve şifre istedi. bizde binding. ile kullanıcının gireceği email ve şifreyi buraya verdik. auth.createUserWithEmailAndPassword(email,password) burası tamamlandıktan sonra tekrar . dedik ve bize sunucunun bize döndüreceği cevaplara göre aksiyon alabileceğimiz listenerlar çıktı. biz de sunucuya gönderilen cevap başarılı olursa ne yapacağız(.addOnSuccessListener(new OnSuccessListener) ve başarısız olursa ne yapacağız durumlarındaki listener'ları ekledik. Aşağısını kendi doldurdu yani ben bir şey yapmadım
 
                 @Override
-                public void onSuccess(AuthResult authResult) {//sunucudan başarılı bir cevap gelirse ne yapacağımız kısmı. Bu method biz server'dan cevabı aldığımızda çalışıyor ve aynı zamanda addOnSuccessListener'da asenkron çalıştığından kullanıcının arayüzüne bir şey yapmış olmuyor.
+                public void onSuccess(AuthResult authResult) {
 
                     Intent intent=new Intent(MainActivity.this,FeedActivity.class);
                     startActivity(intent);
-                    finish();//bu aktiviteyle işim bitince burayı kapatacağım ve bir daha buraya geri dönmeye gerek yok. Kullanıcı giriş yaptıktan sonra bu aktiviteyi kapatayım ki boşu boşuna hafızada yer kaplamasın mesela.
+                    finish();
 
                 }
-            }).addOnFailureListener(new OnFailureListener() {//eğer firebase sunucusuna kayıt için yollanılan şeye dönüş başarısız yönünde olursa ne yapıcaz kısmı
+            }).addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
 
-                    Toast.makeText(MainActivity.this,e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();//e.getLocalizedMessage() demek kullanıcının anlayacağı dilden bir mesaj yaz demektir.
+                    Toast.makeText(MainActivity.this,e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                 }
             });
 
